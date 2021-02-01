b4df03e347d8328b1e5427ce813663e80c9f1c4a
Michael Fatemi <myfatemi04@gmail.com>
2021-01-31 22:38:41 -0500

    Make __DEV__ detection automatic

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const __DEV__ = true;
+export const __DEV__ = process.env.NODE_ENV === 'development';
 
 export const API_SERVER_URL = __DEV__ ? 'http://localhost:5000/' : 'http://spaces-api-999.sites.tjhsst.edu';
 export const UI_SERVER_URL = __DEV__ ? 'http://localhost:3000/' : 'http://spaces.sites.tjhsst.edu';
