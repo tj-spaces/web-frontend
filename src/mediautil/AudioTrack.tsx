e971da2fa3c1b84a0d64abe7a5e915e257c56d6c
Michael Fatemi <myfatemi04@gmail.com>
2021-02-07 16:09:36 -0500

    Clean up AudioTrack code

--- src/mediautil/AudioTrack.tsx
+++ src/mediautil/AudioTrack.tsx
@@ -1,29 +1,25 @@
-import { useContext, useEffect, useRef } from 'react';
-import { AudioTrack as TwilioAudioTrack, AudioTrackPublication } from 'twilio-video';
+import {
+	AudioTrack as TwilioAudioTrack,
+	AudioTrackPublication,
+} from 'twilio-video';
 import useMediaStreamTrack from '../hooks/useMediaStreamTrack';
 import useTrack from '../hooks/useTrack';
-import { defaultPannerNodeSettings } from '../lib/defaultPannerNodeSettings';
-import SpaceAudioContext from '../components/Space/SpaceAudioContext';
 
-export default function AudioTrack({ publication }: { publication?: AudioTrackPublication }) {
+export default function AudioTrack({
+	publication,
+}: {
+	publication?: AudioTrackPublication;
+}) {
 	const audioTrack = useTrack(publication) as TwilioAudioTrack;
-	const audioContext = useContext(SpaceAudioContext);
-	const pannerNode = useRef<PannerNode>();
 	const mediaStreamTrack = useMediaStreamTrack(audioTrack);
 
-	// [audioContext, audioTrack]
-	useEffect(() => {
-		if (mediaStreamTrack) {
-			let audioSource = audioContext.createMediaStreamSource(new MediaStream([mediaStreamTrack]));
-			pannerNode.current = new PannerNode(audioContext, defaultPannerNodeSettings);
-			audioSource.connect(pannerNode.current).connect(audioContext.destination);
-
-			return () => {
-				audioSource.disconnect();
-				pannerNode.current = undefined;
-			};
-		}
-	}, [audioContext, mediaStreamTrack]);
-
-	return null;
+	return (
+		<audio
+			ref={(ref) => {
+				if (mediaStreamTrack && ref) {
+					ref.srcObject = new MediaStream([mediaStreamTrack]);
+				}
+			}}
+		/>
+	);
 }
