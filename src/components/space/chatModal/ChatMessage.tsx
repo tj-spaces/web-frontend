685db5d6ca751a09317529cda351abc2aed2e166
Michael Fatemi <myfatemi04@gmail.com>
2021-02-09 01:13:23 -0500

    Make chat messages show participant display names

--- src/components/space/chatModal/ChatMessage.tsx
+++ src/components/space/chatModal/ChatMessage.tsx
@@ -1,12 +1,17 @@
+import {useContext} from 'react';
 import {SpaceMessage} from '../../../typings/Space';
 import BaseRow from '../../base/BaseRow';
 import BaseText from '../../base/BaseText';
+import SpaceParticipantsContext from '../SpaceParticipantsContext';
 import ChatMessageReplies from './ChatMessageReplies';
 
 export default function ChatMessage({message}: {message: SpaceMessage}) {
+	const participants = useContext(SpaceParticipantsContext);
 	return (
 		<BaseRow direction="column">
-			<BaseText variant="body-bold">Sent by {message.senderID}</BaseText>
+			<BaseText variant="body-bold">
+				Sent by {participants[message.senderID].displayName}
+			</BaseText>
 			<BaseText variant="body">{message.text}</BaseText>
 			{message.replies.length > 0 && (
 				<ChatMessageReplies replies={message.replies} />
