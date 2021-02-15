3b944c7c92b7387020762e05f4ffa06458442990
Michael Fatemi <myfatemi04@gmail.com>
2021-02-15 17:20:45 -0500

    Add basic AccountPage

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import './App.css';
 import AuthenticationWrapper from './components/AuthenticationWrapper';
 import CenteredLoadingText from './components/CenteredLoadingText';
 import ErrorBoundary from './components/ErrorBoundary';
+import StreamerModeContext from './components/StreamerModeContext';
 import ThemeProvider from './ThemeProvider';
 
 const AuthorizationCallback = lazy(
 	() => import('./pages/AuthenticationCallback/AuthenticationCallback')
 );
+const AccountPage = lazy(() => import('./pages/AccountPage/AccountPage'));
 const ClusterPage = lazy(() => import('./pages/ClusterPage/ClusterPage'));
 const ClusterExplorerPage = lazy(
 	() => import('./pages/ClusterExplorerPage/ClusterExplorerPage')
@@ -25,25 +27,32 @@ function App() {
 	return (
 		<ErrorBoundary>
 			<AuthenticationWrapper>
-				<BrowserRouter>
-					<ThemeProvider>
-						<Suspense fallback={<CenteredLoadingText />}>
-							<Switch>
-								<Route
-									path="/auth/:provider/callback"
-									component={AuthorizationCallback}
-								/>
-								<Route path="/spaces/:spaceId" component={SpacePage} />
-								<Route path="/clusters/:clusterId" component={ClusterPage} />
-								<Route path="/login" exact component={LoginPage} />
-								<Route path="/logout" exact component={Logout} />
-								<Route path="/explore" exact component={ClusterExplorerPage} />
-								<Route path="/terms" exact component={TermsPage} />
-								<Route path="/" exact component={DefaultPage} />
-							</Switch>
-						</Suspense>
-					</ThemeProvider>
-				</BrowserRouter>
+				<StreamerModeContext.Provider value={false}>
+					<BrowserRouter>
+						<ThemeProvider>
+							<Suspense fallback={<CenteredLoadingText />}>
+								<Switch>
+									<Route path="/account" component={AccountPage} />
+									<Route
+										path="/auth/:provider/callback"
+										component={AuthorizationCallback}
+									/>
+									<Route path="/spaces/:spaceId" component={SpacePage} />
+									<Route path="/clusters/:clusterId" component={ClusterPage} />
+									<Route path="/login" exact component={LoginPage} />
+									<Route path="/logout" exact component={Logout} />
+									<Route
+										path="/explore"
+										exact
+										component={ClusterExplorerPage}
+									/>
+									<Route path="/terms" exact component={TermsPage} />
+									<Route path="/" exact component={DefaultPage} />
+								</Switch>
+							</Suspense>
+						</ThemeProvider>
+					</BrowserRouter>
+				</StreamerModeContext.Provider>
 			</AuthenticationWrapper>
 		</ErrorBoundary>
 	);
