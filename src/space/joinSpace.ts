2c1523cdeece12d842d5b878d977235422561c59
Michael Fatemi <myfatemi04@gmail.com>
2021-02-17 14:00:34 -0500

    Add joinSpace.ts

---
+++ src/space/joinSpace.ts
@@ -0,0 +1,30 @@
+import {getSpaceJoinCode} from '../api/spaces';
+import {SIM_SERVER_URL} from '../lib/constants';
+
+export default async function joinSpace(
+	id: string
+): Promise<{connection: WebSocket; twilioGrant: string}> {
+	const {code, twilioGrant} = await getSpaceJoinCode(id);
+	const ws = new WebSocket(SIM_SERVER_URL);
+
+	return new Promise((resolve, reject) => {
+		ws.send('connect:' + code);
+
+		const onMessage = (ev: MessageEvent<string>) => {
+			// Immediately remove this event hook after the first message
+			ws.removeEventListener('message', onMessage);
+
+			if (ev.data === 'auth') {
+				resolve({connection: ws, twilioGrant});
+			} else if (ev.data === 'noauth') {
+				reject({error: 'noauth'});
+			} else {
+				reject({error: 'invalid_message_event'});
+			}
+		};
+
+		ws.addEventListener('message', onMessage);
+	});
+}
+
+console.log(joinSpace);
