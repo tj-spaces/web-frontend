59290f3fb8a194cc54cd060e4426bf050fb06533
Michael Fatemi <myfatemi04@gmail.com>
2021-03-06 21:04:37 -0500

    Remove 2D space view

--- src/components/space/spaceView2DPixellated/CanvasContext.tsx
+++
@@ -1,8 +0,0 @@
-import {createContext} from 'react';
-
-/**
- * Context containing the width and height of the 2D space's canvas, in rems.
- */
-const CanvasContext = createContext<{width: number; height: number}>(null!);
-
-export default CanvasContext;
