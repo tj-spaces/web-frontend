cbd80c0c3ef73a919cda54601bbfa7fb6700fbe8
Michael Fatemi <myfatemi04@gmail.com>
2021-03-08 11:41:18 -0500

    Use HTTPS for production urls

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,8 +2,8 @@ export const __DEV__ = false; //process.env.NODE_ENV !== 'production';
 
 export const API_SERVER_URL = __DEV__
 	? 'http://localhost:5000'
-	: 'http://api.joinnebula.co';
+	: 'https://api.joinnebula.co';
 export const UI_SERVER_URL = __DEV__
 	? 'http://localhost:3000'
-	: 'http://www.joinnebula.co';
+	: 'https://www.joinnebula.co';
 export const SIM_SERVER_URL = 'ws://localhost:7000';
