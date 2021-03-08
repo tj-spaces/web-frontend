86b8462afc50d52af3a7ed57d29920f7e7300bbe
Michael Fatemi <myfatemi04@gmail.com>
2021-03-08 11:36:36 -0500

    Use UI server URL for logging in

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,9 @@
 export const __DEV__ = false; //process.env.NODE_ENV !== 'production';
 
 export const API_SERVER_URL = __DEV__
-	? 'http://localhost:5000/'
-	: 'http://api.joinnebula.co/';
+	? 'http://localhost:5000'
+	: 'http://api.joinnebula.co';
 export const UI_SERVER_URL = __DEV__
-	? 'http://localhost:3000/'
-	: 'http://www.joinnebula.co/';
+	? 'http://localhost:3000'
+	: 'http://www.joinnebula.co';
 export const SIM_SERVER_URL = 'ws://localhost:7000';
--- src/lib/getAuthorizationUrl.ts
+++ src/lib/getAuthorizationUrl.ts
@@ -1,10 +1,12 @@
+import {UI_SERVER_URL} from './constants';
+
 export function getIonAuthorizationUrl(): string {
 	const url = new URL('https://ion.tjhsst.edu/oauth/authorize');
 
 	url.searchParams.set('response_type', 'code');
 	url.searchParams.set('scope', 'read');
 	url.searchParams.set('client_id', 'ScL9QdZ9m3iGmHG11uznwABg4ZSkabQJan05ZYsk');
-	url.searchParams.set('redirect_uri', 'http://localhost:3000/auth/ion/callback');
+	url.searchParams.set('redirect_uri', UI_SERVER_URL + '/auth/ion/callback');
 
 	return url.toString();
 }
@@ -16,11 +18,17 @@ export function getGoogleAuthorizationUrl(): string {
 	url.searchParams.set('prompt', 'consent');
 	url.searchParams.set(
 		'scope',
-		['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'].join(' ')
+		[
+			'https://www.googleapis.com/auth/userinfo.profile',
+			'https://www.googleapis.com/auth/userinfo.email',
+		].join(' ')
 	);
 	url.searchParams.set('response_type', 'code');
-	url.searchParams.set('client_id', '368145913097-9ttib28rs8o6e9iqulr7mg2t0qvst28i.apps.googleusercontent.com');
-	url.searchParams.set('redirect_uri', 'http://localhost:3000/auth/google/callback');
+	url.searchParams.set(
+		'client_id',
+		'368145913097-9ttib28rs8o6e9iqulr7mg2t0qvst28i.apps.googleusercontent.com'
+	);
+	url.searchParams.set('redirect_uri', UI_SERVER_URL + '/auth/google/callback');
 
 	return url.toString();
 }
