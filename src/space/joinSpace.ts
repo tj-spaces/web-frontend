e43a02b2550a76d2a5161885af3593ce8fd4e1ea
Michael Fatemi <myfatemi04@gmail.com>
2021-03-14 19:48:43 -0400

    Add option to disable simulation server SSL

--- src/space/joinSpace.ts
+++ src/space/joinSpace.ts
@@ -6,6 +6,7 @@
 */
 import {getSpaceJoinCode} from '../api/spaces';
 import {
+	DISABLE_DEV_SIMULATION_SERVER_SSL,
 	USE_DEV_SIMULATION_SERVER,
 	USE_DEV_VOICE_SERVER,
 } from '../lib/constants';
@@ -19,7 +20,9 @@ export default async function joinSpace(
 		: joinInfo.simulationURL;
 
 	const voiceURL = USE_DEV_VOICE_SERVER ? 'localhost:8080' : joinInfo.voiceURL;
-	const ws = new WebSocket('wss://' + simulationURL);
+	const ws = new WebSocket(
+		(DISABLE_DEV_SIMULATION_SERVER_SSL ? 'ws' : 'wss') + '://' + simulationURL
+	);
 
 	return new Promise((resolve, reject) => {
 		ws.onopen = () => {
