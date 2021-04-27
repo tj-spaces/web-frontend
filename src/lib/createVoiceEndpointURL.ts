acdac09f338c0f2bbb1ee06b83675ad217d119a9
Michael Fatemi <myfatemi04@gmail.com>
2021-04-27 12:04:31 -0400

    Add createVoiceEndpointURL

---
+++ src/lib/createVoiceEndpointURL.ts
@@ -0,0 +1,9 @@
+import {USE_VOICE_SERVER_SSL} from './constants';
+
+export default function createVoiceEndpointURL(url: string) {
+	const wssRegex = /^wss?:\/\//;
+	if (!wssRegex.test(url)) {
+		return (USE_VOICE_SERVER_SSL ? 'wss://' : 'ws://') + url + '/websocket';
+	}
+	return url;
+}
