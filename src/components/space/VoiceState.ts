b43ba95a3611c1e3cb10cf0e944f42c6941c95c1
Michael Fatemi <myfatemi04@gmail.com>
2021-04-27 14:19:41 -0400

    Update VoiceState

--- src/components/space/VoiceState.ts
+++ src/components/space/VoiceState.ts
@@ -1,31 +1,46 @@
-import {Record, Map} from 'immutable';
+import {Record, Map, Set} from 'immutable';
 import RTCUser from './RTCUser';
 import VoiceImmutableMediaTrack from './VoiceImmutableMediaTrack';
 
 export type VoiceStateProps = {
-	tracks: Map<string, VoiceImmutableMediaTrack>;
-	rtcUsers: Map<string, RTCUser>;
-	localUser: RTCUser;
+	streams: Map<string, Set<VoiceImmutableMediaTrack>>;
+	users: Map<string, RTCUser>;
 };
 
 export default class VoiceState extends Record<VoiceStateProps>({
-	tracks: Map(),
-	rtcUsers: Map(),
-	localUser: new RTCUser({}),
+	streams: Map(),
+	users: Map(),
 }) {
-	getUserTracks(userId: string) {
-		const user = this.rtcUsers.get(userId);
-		if (user) {
-			const tracks: VoiceImmutableMediaTrack[] = [];
-			user.streamIDs.forEach((trackID) => {
-				const track = this.tracks.get(trackID);
-				if (track) {
-					tracks.push(track);
-				}
-			});
-			return tracks;
-		} else {
-			return null;
+	private addTrackToStream(streamID: string, track: VoiceImmutableMediaTrack) {
+		const stream = this.streams.get(streamID, Set()).add(track);
+		return this.set('streams', this.streams.set(streamID, stream));
+	}
+	private addStreamToUser(streamID: string, userID: string) {
+		return this.set(
+			'users',
+			this.users.set(
+				userID,
+				this.users.get(userID, new RTCUser({id: userID})).addStreamID(streamID)
+			)
+		);
+	}
+	addTrack(track: VoiceImmutableMediaTrack, streamID: string, userID: string) {
+		let state = this;
+		if (!state.streams.has(streamID)) {
+			state.addStream(streamID, userID);
 		}
+		return state.addTrackToStream(streamID, track);
+	}
+	addStream(streamID: string, userID: string) {
+		let state = this;
+		state = state.set('streams', state.streams.set(streamID, Set()));
+		state = state.addStreamToUser(streamID, userID);
+		return state;
+	}
+	addLocalUserTrack(track: VoiceImmutableMediaTrack) {
+		return this.addTrack(track, '@me:user', '@me');
+	}
+	addLocalScreenTrack(track: VoiceImmutableMediaTrack) {
+		return this.addTrack(track, '@me:screen', '@me');
 	}
 }
