f1ef2a3dd5272bb5fa763157ffb46632a48b5d31
Michael Fatemi <myfatemi04@gmail.com>
2021-05-03 00:32:32 -0400

    update packet formats in signaling channel

--- src/components/space/SignalingChannel.ts
+++ src/components/space/SignalingChannel.ts
@@ -20,13 +20,13 @@ export default class SignalingChannel {
 		Set<SubscriptionStateListener>
 	>();
 
-	constructor(signalingUrl: string) {
+	constructor(
+		signalingUrl: string,
+		helloInfo: {userID: string; role: 'publisher' | 'subscriber'}
+	) {
 		this.websocket = new WebSocket(signalingUrl);
 		this.websocket.onopen = () => {
-			this.messageQueue.forEach(([eventName, eventData]) => {
-				this._send(eventName, eventData);
-			});
-			this.messageQueue = [];
+			this._send('hello', `${helloInfo.userID}:${helloInfo.role}`);
 		};
 		this.websocket.onmessage = (event) => {
 			const message = JSON.parse(event.data);
@@ -43,14 +43,23 @@ export default class SignalingChannel {
 
 	private _handleMessage(eventName: string, eventData: any) {
 		switch (eventName) {
-			case 'rtc_ice_candidate':
-				this.iceCandidateHandlers.forEach((listener) => listener(eventData));
+			case 'rtc_subscriber_candidate':
+			case 'rtc_publisher_candidate': {
+				const candidate = new RTCIceCandidate(JSON.parse(eventData));
+				this.iceCandidateHandlers.forEach((listener) => listener(candidate));
 				break;
+			}
 
-			case 'rtc_offer':
-			case 'rtc_answer':
-				this.sdpHandlers.forEach((listener) => listener(eventData));
+			case 'rtc_subscriber_offer':
+			case 'rtc_publisher_answer': {
+				const sdp = new RTCSessionDescription(JSON.parse(eventData));
+				const handlers = this.sdpHandlers;
+				if (handlers.size === 0) {
+					AirwaveLoggerGlobal.warn('No handlers registered to handle SDP');
+				}
+				this.sdpHandlers.forEach((listener) => listener(sdp));
 				break;
+			}
 
 			case 'rtc_subscription_update': {
 				const {streamID, constraints}: SubscriptionDescriptor = eventData;
@@ -61,6 +70,23 @@ export default class SignalingChannel {
 				}
 				break;
 			}
+
+			case 'hello': {
+				this.messageQueue.forEach(([eventName, eventData]) => {
+					this._send(eventName, eventData);
+				});
+				this.messageQueue = [];
+				break;
+			}
+			case 'goodbye':
+				AirwaveLoggerGlobal.mustfix('Received goodbye message');
+				break;
+
+			default:
+				AirwaveLoggerGlobal.mustfix(
+					'Received unusual eventName: %p',
+					eventName
+				);
 		}
 	}
 
@@ -88,7 +114,13 @@ export default class SignalingChannel {
 			} else {
 				const onTimeout = () => reject('timeout');
 				const nonce = Math.random().toString(36).slice(2);
-				this.websocket.send(JSON.stringify({nonce, event, data}));
+				this.websocket.send(
+					JSON.stringify({
+						nonce,
+						event,
+						data: typeof data === 'string' ? data : JSON.stringify(data),
+					})
+				);
 				this.ackHandlers.set(nonce, (event: string, data?: any) => {
 					this.timeoutHandlers.delete(onTimeout);
 					resolve({event, data});
@@ -99,23 +131,42 @@ export default class SignalingChannel {
 	}
 
 	sendOffer(offer: RTCSessionDescriptionInit) {
-		this._send('rtc_offer', offer);
+		this._send('rtc_publisher_offer', offer);
 	}
 
 	sendAnswer(answer: RTCSessionDescriptionInit) {
-		this._send('rtc_answer', answer);
+		this._send('rtc_subscriber_answer', answer);
 	}
 
-	sendIceCandidate(candidate: RTCIceCandidateInit) {
-		this._send('rtc_ice_candidate', candidate);
+	sendIceCandidate(
+		candidate: RTCIceCandidateInit,
+		type: 'subscriber' | 'publisher'
+	) {
+		this._send(`rtc_${type}_ice_candidate`, candidate);
+	}
+
+	private serializeSubscriptionUpdate(
+		streamID: string,
+		constraints: SubscriptionState
+	) {
+		return `${streamID} ${(['audio', 'video'] as const)
+			.filter((val) => !!constraints[val])
+			.join(',')}`;
 	}
 
 	sendSubscriptionUpdate(streamID: string, constraints: SubscriptionState) {
-		this._send('rtc_subscription_update_request', {streamID, constraints});
+		this._send(
+			'rtc_subscription_update_request',
+			this.serializeSubscriptionUpdate(streamID, constraints)
+		);
 	}
 
 	sendCloseAllSubscriptions() {
 		this._send('rtc_close_connection', '');
+	}
+
+	sendHelloMessage(userID: string, role: 'publisher' | 'subscriber') {
+		this._send('hello', `${userID}:${role}`);
 	}
 
 	onIceCandidate(fn: IceCandidateHandler) {
