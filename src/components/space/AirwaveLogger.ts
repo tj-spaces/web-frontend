2eb0c9a4e4e3bc4c47ca52c547e74d6d052f5886
Michael Fatemi <myfatemi04@gmail.com>
2021-05-08 15:08:07 -0400

    Add [AW] prefix to AirwaveLogger

--- src/components/space/AirwaveLogger.ts
+++ src/components/space/AirwaveLogger.ts
@@ -2,26 +2,26 @@ import formatMessage from '../../lib/formatMessage';
 
 class AirwaveLogger {
 	mustfix(messageFormat: string, ...params: any[]) {
-		console.error(formatMessage(messageFormat, ...params));
+		console.error('[AW]', formatMessage(messageFormat, ...params));
 	}
 	error(messageFormat: string, ...params: any[]) {
-		console.error(formatMessage(messageFormat, ...params));
+		console.error('[AW]', formatMessage(messageFormat, ...params));
 	}
 	warn(messageFormat: string, ...params: any[]) {
-		console.warn(formatMessage(messageFormat, ...params));
+		console.warn('[AW]', formatMessage(messageFormat, ...params));
 	}
 	info(messageFormat: string, ...params: any[]) {
-		console.log(formatMessage(messageFormat, ...params));
+		console.log('[AW]', formatMessage(messageFormat, ...params));
 	}
 	debug(messageFormat: string, ...params: any[]) {
-		console.log(formatMessage(messageFormat, ...params));
+		console.log('[AW]', formatMessage(messageFormat, ...params));
 	}
 	checkpoint(checkpoint: string, messageFormat?: string, ...params: any[]) {
 		if (!messageFormat) {
-			console.log('Reached checkpoint', checkpoint);
+			console.log('[AW] Reached checkpoint', checkpoint);
 		} else {
 			console.log(
-				`Reached checkpoint ${checkpoint}:`,
+				`[AW] Reached checkpoint ${checkpoint}:`,
 				formatMessage(messageFormat, ...params)
 			);
 		}
