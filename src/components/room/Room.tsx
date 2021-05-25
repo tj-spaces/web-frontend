4361ab4c434533b27b5c65296404aee904f34579
Michael Fatemi <myfatemi04@gmail.com>
2021-05-25 01:02:37 -0400

    fix useinterval hook

--- src/components/room/Room.tsx
+++ src/components/room/Room.tsx
@@ -1,11 +1,17 @@
 import VoiceProvider from '@airwave/VoiceProvider';
 import {useParams} from 'react-router';
+import useInterval from 'src/hooks/useInterval';
 import useSet from 'src/hooks/useSet';
 import RoomPresence from './RoomPresence';
 
 export default function Room() {
 	const {roomID} = useParams<{roomID: string}>();
 	const users = useSet<string>();
+
+	useInterval(() => {
+		users.add('hello' + new Date().getTime());
+		console.log('adding hello');
+	}, 1000);
 
 	return (
 		<VoiceProvider voiceURL="localhost">
--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -1,9 +1,19 @@
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 
 export default function useInterval(callback: () => void, ms: number) {
-	useEffect(() => {
-		const interval = setInterval(callback, ms);
+	// We use this so we don't need to set/clear the interval every time
+	// the callback changes
+	const callbackRef = useRef(callback);
 
-		return clearInterval(interval);
-	});
+	useEffect(() => {
+		callbackRef.current = callback;
+	}, [callback]);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			callbackRef.current();
+		}, ms);
+
+		return () => clearInterval(interval);
+	}, [ms]);
 }
